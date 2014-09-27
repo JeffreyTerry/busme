36992eb7750c2aa5d653733884eb415123468a67
JeffreyTerry <jterry94@gmail.com>
2014-09-27 00:03:00 -0400

    created all the layout stuff bro

--- src/com/example/busme/MainActivity.java
+++ src/com/example/busme/MainActivity.java
@@ -3,33 +3,70 @@ package com.example.busme;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	private MainController mainController;
+	private ListView mainListView;
+	private EditText etDestination, etStart;
+	private View shadowExpanded, shadowRetracted, mainEtDivider;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+		// WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		mainController = new MainController(this);
 
+		setContentView(R.layout.activity_main);
+		etStart = (EditText) findViewById(R.id.etStart);
+		etDestination = (EditText) findViewById(R.id.etDestination);
+		shadowExpanded = findViewById(R.id.shadowExpanded);
+		shadowRetracted = findViewById(R.id.shadowRetracted);
+		mainEtDivider = findViewById(R.id.mainEtDivider);
+		mainListView = (ListView) findViewById(R.id.lvMain);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		mainController.setEtStart(etStart);
+		mainController.setEtDestination(etDestination);
+		mainController.setListView(mainListView);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the options menu from XML
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+
+		return true;
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mainController.resetLocationUpdateCount();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void showEtStart(View v) {
+		shadowRetracted.setVisibility(View.INVISIBLE);
+		shadowExpanded.setVisibility(View.VISIBLE);
+		etStart.setVisibility(View.VISIBLE);
+		mainEtDivider.setVisibility(View.VISIBLE);
+		etDestination.setCursorVisible(true);
+	}
 }
