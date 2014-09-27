74706e1d1c89126c489cc958d88b218e23ebc6ef
JeffreyTerry <jterry94@gmail.com>
2014-09-27 15:06:37 -0400

    fixed keyboard popout

--- src/com/example/busme/MainViewListFragment.java
+++ src/com/example/busme/MainViewListFragment.java
@@ -7,10 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 
 public class MainViewListFragment extends Fragment {
@@ -37,10 +37,12 @@ public class MainViewListFragment extends Fragment {
 		mainListView = (ListView) android.findViewById(R.id.lvMain);
 
 		// hide the keyboard
+		etStart.clearFocus();
+		etDestination.clearFocus();
 		InputMethodManager imm = (InputMethodManager) mainActivity
 				.getSystemService(Context.INPUT_METHOD_SERVICE);
-		imm.hideSoftInputFromWindow(etStart.getWindowToken(), 0);
-		imm.hideSoftInputFromWindow(etDestination.getWindowToken(), 0);
+		mainActivity.getWindow().setSoftInputMode(
+				WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
 		mainController.setEtStart(etStart);
 		mainController.setEtDestination(etDestination);
